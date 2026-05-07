a9086bb0af9cb410e1bb5278d316b74359fece33
s21120241-star <s21120241@alumnos.itsur.edu.mx>
2026-05-06 23:28:58 -0600

    Implementacion de imagenes y pasos anteriores

--- KotlinMultiplatform/composeApp/src/commonMain/kotlin/com/example/kotlinmultiplatform/App.kt
+++ KotlinMultiplatform/composeApp/src/commonMain/kotlin/com/example/kotlinmultiplatform/App.kt
@@ -1,20 +1,18 @@
 package com.example.kotlinmultiplatform
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,17 +21,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.jetbrains.compose.resources.painterResource
-
 import kotlinmultiplatform.composeapp.generated.resources.Res
-import kotlinmultiplatform.composeapp.generated.resources.compose_multiplatform
+import kotlinmultiplatform.composeapp.generated.resources.eg
+import kotlinmultiplatform.composeapp.generated.resources.fr
+import kotlinmultiplatform.composeapp.generated.resources.id
+import kotlinmultiplatform.composeapp.generated.resources.jp
+import kotlinmultiplatform.composeapp.generated.resources.mx
 import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 import kotlin.time.Clock
-
-data class Country(val name: String, val zone: TimeZone)
+data class Country(val name: String, val zone: TimeZone, val image: DrawableResource)
 
 fun currentTimeAt(location: String, zone: TimeZone): String {
     fun LocalTime.formatted() = "$hour:$minute:$second"
@@ -44,21 +45,19 @@ fun currentTimeAt(location: String, zone: TimeZone): String {
     return "The time in $location is ${localTime.formatted()}"
 }
 
-fun countries() = listOf(
-    Country("Japan", TimeZone.of("Asia/Tokyo")),
-    Country("France", TimeZone.of("Europe/Paris")),
-    Country("Mexico", TimeZone.of("America/Mexico_City")),
-    Country("Indonesia", TimeZone.of("Asia/Jakarta")),
-    Country("Egypt", TimeZone.of("Africa/Cairo")),
+val defaultCountries = listOf(
+    Country("Japan", TimeZone.of("Asia/Tokyo"), Res.drawable.jp),
+    Country("France", TimeZone.of("Europe/Paris"), Res.drawable.fr),
+    Country("Mexico", TimeZone.of("America/Mexico_City"), Res.drawable.mx),
+    Country("Indonesia", TimeZone.of("Asia/Jakarta"), Res.drawable.id),
+    Country("Egypt", TimeZone.of("Africa/Cairo"), Res.drawable.eg)
 )
 
 @Composable
 @Preview
-fun App(countries: List<Country> = countries()) {
+fun App(countries: List<Country> = defaultCountries) {
     MaterialTheme {
-
         var showCountries by remember { mutableStateOf(false) }
-        var location by remember { mutableStateOf("Europe/Paris") }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
         Column(
@@ -78,9 +77,16 @@ fun App(countries: List<Country> = countries()) {
                     expanded = showCountries,
                     onDismissRequest = { showCountries = false }
                 ) {
-                    countries().forEach { (name, zone) ->
+                    countries.forEach { (name, zone, image) ->
                         DropdownMenuItem(
-                            text = {   Text(name)},
+                            text = { Row(verticalAlignment = Alignment.CenterVertically) {
+                                Image(
+                                    painterResource(image),
+                                    modifier = Modifier.size(50.dp).padding(end = 10.dp),
+                                    contentDescription = "$name flag"
+                                )
+                                Text(name)
+                            } },
                             onClick = {
                                 timeAtLocation = currentTimeAt(name, zone)
                                 showCountries = false
@@ -93,10 +99,6 @@ fun App(countries: List<Country> = countries()) {
             Button(modifier = Modifier.padding(start = 20.dp, top = 10.dp),
                 onClick = { showCountries = !showCountries }) {
                 Text("Select Location")
-            Text(timeAtLocation)
-            TextField(value = location, onValueChange = { location = it })
-            Button(onClick = { timeAtLocation = "13:30" }) {
-                Text("Show Time At Location")
             }
         }
     }
